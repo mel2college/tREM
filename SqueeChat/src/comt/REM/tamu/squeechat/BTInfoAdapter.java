c88e4dbc6261705879cf0f89b33ca909aea54b04
Isaac Grande <igrande0@gmail.com>
2014-02-15 17:41:23 -0600

    wrote all arduino setup code and most of Android setup code

---
+++ SqueeChat/src/comt/REM/tamu/squeechat/BTInfoAdapter.java
@@ -0,0 +1,75 @@
+package comt.REM.tamu.squeechat;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class BTInfoAdapter extends BaseAdapter {
+	private ArrayList<String> NameList;
+	private ArrayList<String> AddressList;
+	private LayoutInflater mInflater;
+	
+	static class ViewHolder {
+		TextView text1;
+		TextView text2;
+	}
+
+	public BTInfoAdapter(Context c, ArrayList<String> Names, ArrayList<String> Addresses) {
+		NameList = Names;
+		AddressList = Addresses;
+		
+		mInflater = LayoutInflater.from(c);
+	}
+	
+	public void updateAdapter(ArrayList<String> NewNames, ArrayList<String> NewAddresses){
+		NameList.clear();
+		AddressList.clear();
+		NameList.addAll(NewNames);
+		AddressList.addAll(NewAddresses);
+		notifyDataSetChanged();
+	}
+	
+	@Override
+	public int getCount() {
+		return AddressList.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return AddressList.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		ViewHolder holder;
+		
+		if(convertView == null) {
+			convertView = mInflater.inflate(android.R.layout.simple_list_item_2, null);
+			
+			holder = new ViewHolder();
+			holder.text1 = (TextView)convertView.findViewById(android.R.id.text1);
+			holder.text2 = (TextView)convertView.findViewById(android.R.id.text2);
+			
+			convertView.setTag(holder);
+		}
+		else {
+			holder = (ViewHolder)convertView.getTag();
+		}
+	
+		holder.text1.setText(NameList.get(position));
+		holder.text2.setText(AddressList.get(position));
+		
+		return convertView;
+	}
+
+}
--- SqueeChat/src/comt/REM/tamu/squeechat/MainActivity.java
+++ SqueeChat/src/comt/REM/tamu/squeechat/MainActivity.java
@@ -1,15 +1,81 @@
 package comt.REM.tamu.squeechat;
 
+import java.io.OutputStream;
+import java.util.ArrayList;
+
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
+	private BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+	private BluetoothSocket mBluetoothSocket;
+	private OutputStream outStream;
+	private ArrayList<String> names;
+	private ArrayList<String> addresses;
+	private BTInfoAdapter mAdapter = new BTInfoAdapter(this, names, addresses);
+	private IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+	private ListView mListView;
+	
+	private static final int REQUEST_ENABLE_BT = 1;
+	
+	// Create a BroadcastReceiver for ACTION_FOUND
+	public final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			String action = intent.getAction();
+			// When discovery finds a device
+			if(BluetoothDevice.ACTION_FOUND.equals(action)){
+				// get the BluetoothDevice object from the intent
+				BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+				// add the name and address to an array adapter to show in a ListView
+				names.add(device.getName());
+				addresses.add(device.getAddress());
+				mAdapter.updateAdapter(names, addresses);
+			}
+		}
+	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		mListView = (ListView) findViewById(R.id.list_view);
+		mListView.setAdapter(mAdapter);
+		registerReceiver(mReceiver, filter);
+		
+		// device does not support bluetooth, quit application
+		if(mBluetoothAdapter == null) {
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setMessage("Your device does not support Bluetooth.");
+			builder.setCancelable(false);
+			builder.setPositiveButton("Quit", new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					finish();
+				}
+			});
+			builder.create().show();
+		}
+		if(!mBluetoothAdapter.isEnabled()){
+			Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+			startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+		}
+		
+		mBluetoothAdapter.startDiscovery();
 	}
 
 	@Override
@@ -18,5 +84,37 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
+	
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		
+		switch(requestCode){
+		case REQUEST_ENABLE_BT:
+			if(resultCode == Activity.RESULT_CANCELED){
+				AlertDialog.Builder builder = new AlertDialog.Builder(this);
+				builder.setMessage("Bluetooth was not enabled. Enable now?");
+				builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+						startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+					}
+				});
+				builder.setNegativeButton("Quit", new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						finish();
+					}
+				});
+				builder.create().show();
+			}
+			break;
+		}
+	}
+	
+	protected void onDestroy() {
+		super.onDestroy();
+		
+		unregisterReceiver(mReceiver);
+	}
 }
