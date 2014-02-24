3e5d5c7b027a65ed8a225854d050fdf61a128f93
Isaac Grande <igrande0@gmail.com>
2014-02-24 15:59:42 -0600

    fixes to arduino code

--- BLUNO_DEMO/BLUNO_DEMO.ino
+++ BLUNO_DEMO/BLUNO_DEMO.ino
@@ -14,8 +14,8 @@ void loop()
     {
         char command[128];  
         char buf;
-        int index;
-        int pos;
+        int index = 0;
+        int pos = 0;
         
         while(true){
           buf = Serial.read();
@@ -24,13 +24,15 @@ void loop()
           command[index] = buf;
           ++index;
         }
+        //command[index]='\0';
+        --index;
         
         for(int i = 0; i < index; ++i){
-          pos += (command[index-i]-'0')*pow(10,0);
+          pos += (command[index-i]-'0')*(int)pow(10,i);
         }
         
         mServo.write(pos);
-        Serial.write("Servo to position " + pos);
+        Serial.write("Servo to position ");
     }
 }
 
