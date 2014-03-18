8dc90ea28986a3153939ccb01318ebe6c4f12a71
Isaac Grande <igrande0@gmail.com>
2014-03-18 13:26:28 -0500

    added start and stop commands; created a squeeze function that uses force sensor readings

--- BLUNO_DEMO/BLUNO_DEMO.ino
+++ BLUNO_DEMO/BLUNO_DEMO.ino
@@ -3,12 +3,15 @@
 
 Servo mServo;
 PlainProtocol comm(Serial, 115200);
+int servo_position;
 
 void setup() {
     comm.init();
     Serial.begin(115200);               //initial the Serial
     
     mServo.attach(5);
+    mServo.write(0);
+    servo_position = 0;
 }
 
 void loop()
@@ -18,6 +21,25 @@ void loop()
     if(comm.receiveFrame()){
       if(comm.receivedCommand == "position"){
         mServo.write(comm.receivedContent[0]);
+        servo_position = comm.receivedContent[0];
+        Serial.println("<success>;");
+      }
+      else if(comm.receivedCommand == "start"){
+        switch(comm.receivedContent[0]){
+         case 0: // strength soft - 10N (~101 from analog read)
+         squeeze(101);
+         break;
+         case 1: // strength medium - 40N (~409 from analog read)
+         squeeze(409);
+         break;
+         case 2: // strength hard - 70N (~716 from analog read)
+         squeeze(716);
+         break;
+        }
+      }
+      else if(comm.receivedCommand == "stop") {
+        mServo.write(0);
+        servo_position = 0;
         Serial.println("<success>;");
       }
       else{
@@ -25,5 +47,23 @@ void loop()
       }
     }
   }
+}
+
+void squeeze(int force_max){
+  int force;
+  while((force = analogRead(A0)) < force_max){
+    Serial.println("<read_force>" + String(force) + ";");
+    Serial.println("<current_position>" + String(servo_position) + ";");
+    
+    if((servo_position + 3) > 180){
+      Serial.println("<failure>;");
+      return; 
+    }
+    
+    // increment motor position by 3 degrees until finding stopping point
+    mServo.write(servo_position + 3);
+    servo_position += 3;
+  }
+  Serial.println("<success>;");
 }
 
