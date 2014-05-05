d78ace52edc1e70ea2781ac6374ffd83212a5a2b
Isaac Grande <igrande0@gmail.com>
2014-05-05 16:08:21 -0500

    cleaned up the Arduino code

--- BLUNO_DEMO/BLUNO_DEMO.ino
+++ BLUNO_DEMO/BLUNO_DEMO.ino
@@ -3,15 +3,13 @@
 
 Servo mServo;
 PlainProtocol comm(Serial, 115200);
-int servo_position;
 
 void setup() {
     comm.init();
-    Serial.begin(115200);               //initial the Serial
+    Serial.begin(115200);
     
     mServo.attach(5);
     mServo.write(0);
-    servo_position = 0;
 }
 
 void loop()
@@ -21,30 +19,9 @@ void loop()
     if(comm.receiveFrame()){
       if(comm.receivedCommand == "position"){
         mServo.write(comm.receivedContent[0]);
-        servo_position = comm.receivedContent[0];
-      }
-      else if(comm.receivedCommand == "start"){
-        switch(comm.receivedContent[0]){
-         case 0: // strength soft - 10N (~101 from analog read)
-         //squeeze(101);
-         mServo.write(90);
-         servo_position = 90;
-         break;
-         case 1: // strength medium - 40N (~409 from analog read)
-         //squeeze(409);
-         mServo.write(135);
-         servo_position = 135;
-         break;
-         case 2: // strength hard - 70N (~716 from analog read)
-         //squeeze(716);
-         mServo.write(180);
-         servo_position = 180;
-         break;
-        }
       }
       else if(comm.receivedCommand == "stop") {
         mServo.write(0);
-        servo_position = 0;
       }
       else if(comm.receivedCommand == "force") {
         int force = analogRead(A0);
@@ -52,22 +29,5 @@ void loop()
       }
     }
   }
-}
-
-void squeeze(int force_max){
-  int force;
-  while((force = analogRead(A0)) < force_max){
-    Serial.println("<read_force>" + String(force) + ";");
-    Serial.println("<current_position>" + String(servo_position) + ";");
-    
-    if((servo_position + 3) > 180){
-      return; 
-    }
-    
-    // increment motor position by 3 degrees until finding stopping point
-    mServo.write(servo_position + 3);
-    servo_position += 3;
-  }
-  Serial.println("<success>;");
 }
 
